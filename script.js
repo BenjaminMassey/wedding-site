13d4c5ede1863d0f905a48da2057066ecfa2c7b6
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-05-04 12:12:45 -0700

    Full rework. Took ben-syd-wedding-placeholder and modified the styling to be like hte invitation rather than the save the date. Implemented 'screen' based system of cross fading between different content. Added top bar for navigation.

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
+let animating = false;
+
+async function tweenOpacity(divName, milliseconds, direction) {
+    let iters = 100;
+    let opacity = document.getElementById(divName).style.opacity;
+    for (let i = 0; i <= iters; i++) {
+        document.getElementById(divName).style.opacity = opacity;
+        opacity -= (1.0 / iters) * direction;
+        opacity = Math.max(0.0, Math.min(opacity, 1.0));
+        await new Promise(r => setTimeout(r, milliseconds / iters));
+    }
+}
+
+async function tweenOn(divName, milliseconds) {
+    tweenOpacity(divName, milliseconds, -1);
+}
+
+async function tweenOff(divName, milliseconds) {
+    tweenOpacity(divName, milliseconds, 1);
+}
+
+function tweenOffAllButOne(excludedDivName, milliseconds) {
+    const screens = document.querySelectorAll('div.screen');
+    function tweenOffDiv(div) {
+        if (div.id != excludedDivName) {
+            tweenOff(div.id);
         }
-    });
-});
+    }
+    screens.forEach(tweenOffDiv);
+}
+
+async function moveOffOthers(divName) {
+    const screens = document.querySelectorAll('div.screen');
+    function moveOff(div) {
+        if (div.id != divName) {
+            div.style.top = "100vh";
+        }
+    }
+    screens.forEach(moveOff);
+}
+
+async function moveBackAll() {
+    const screens = document.querySelectorAll('div.screen');
+    function moveBack(div) {
+        div.style.top = "10vh";
+    }
+    screens.forEach(moveBack);
+}
+
+async function showScreen(divName) {
+    if (animating) {
+        return;
+    }
+    animating = true;
+    moveBackAll();
+    tweenOffAllButOne(divName, 350);
+    await new Promise(r => setTimeout(r, 650));
+    tweenOn(divName, 350);
+    await new Promise(r => setTimeout(r, 800));
+    moveOffOthers(divName);
+    animating = false;
+}
+
+function initialShow() {
+    tweenOn('home', 500);
+}
